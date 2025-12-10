1a81fec8283bce68020ea9fcf9c9789ddf8334d3
Lucas da Silva Oliveira <lucas.oliveira@morningstar.dev.br>
2025-12-10 15:04:26 -0300

    refact: tags looping implementation

--- src/components/Card/CardHome.tsx
+++ src/components/Card/CardHome.tsx
@@ -1,14 +1,14 @@
-import ProjectPhoto from "../../assets/imgs/exemplo-projeto.webp";
-
 interface ICardHomeProps {
     title: string,
-    description: string
+    description: string,
+    tags: string[],
+    imageSrc: string
 }
 
 export default function CardHome(cardHomeProps: ICardHomeProps) {
     return (
         <div className="max-w-sm rounded overflow-hidden shadow-lg bg-gray-800">
-            <img className="w-full" src={ProjectPhoto} alt="Sunset in the mountains" />
+            <img className="w-full" src={cardHomeProps.imageSrc} alt="Sunset in the mountains" />
             <div className="px-6 py-4">
                 <div className="font-bold text-xl mb-2">{cardHomeProps.title}</div>
                 <p className="text-white text-base">
@@ -16,10 +16,14 @@ export default function CardHome(cardHomeProps: ICardHomeProps) {
                 </p>
             </div>
             <div className="px-6 pt-4 pb-2">
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#programação</span>
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#gestão</span>
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#consultoria</span>
-            </div>
+                {cardHomeProps.tags.map((tag) => (
+                    <span
+                        key={tag}
+                        className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+                    >
+                        #{tag}
+                    </span>
+                ))}</div>
         </div>
     );
 }
