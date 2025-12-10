43b59276e2c92341d77fa954a75212d643e514ac
Lucas da Silva Oliveira <lucas.oliveira@morningstar.dev.br>
2025-12-10 13:30:03 -0300

    feat: add baseURL in axios

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import axios from 'axios'
+
+axios.defaults.baseURL = import.meta.env.BASE_URL || 'https://localhost:7264/api'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
