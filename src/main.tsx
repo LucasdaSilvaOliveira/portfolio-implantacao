a3cde4260478bad964591fea4cf542081c3d9c94
Lucas da Silva Oliveira <lucas.oliveira@morningstar.dev.br>
2025-12-16 13:18:12 -0300

    feat: aidiconando base na configuração

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import axios from 'axios'
 
 // axios.defaults.baseURL = 'https://localhost:7264/api'
 
